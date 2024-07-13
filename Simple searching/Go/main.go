7c076dc8c78ea980ff09a52d5cd817f2fed98510
miyakiyu <sherry910509@gmail.com>
2024-07-13 19:41:57 +0800

    Fix Nginx can't currrectly return HTML

--- Simple searching/Go/main.go
+++ Simple searching/Go/main.go
@@ -1,108 +1,97 @@
 package main
 
 import (
-    "database/sql"
-    "encoding/json"
-    "log"
-    "net/http"
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
 
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
-
-type Person struct {
-    ID    int    `json:"id"`
-    Name  string `json:"name"`
-    Email string `json:"email"`
-}
 
 var db *sql.DB
 
-func initDB() {
-    var err error
-    db, err = sql.Open("mysql", "may:12345@tcp(mysql:3306)/db")
-    if err != nil {
-        log.Fatal(err)
-    }
-}
-
 func main() {
-    initDB()
-    http.HandleFunc("/api/person", personHandler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	var err error
+	db, err = sql.Open("mysql", "myuser:mypassword@tcp(mysql:3306)/mydatabase")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	http.HandleFunc("/api/query", queryHandler)
+	http.HandleFunc("/api/add", addHandler)
+	http.HandleFunc("/api/delete", deleteHandler)
+
+	http.Handle("/", http.FileServer(http.Dir("/static")))
+
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func personHandler(w http.ResponseWriter, r *http.Request) {
-    switch r.Method {
-    case http.MethodGet:
-        getPersons(w, r)
-    case http.MethodPost:
-        addPerson(w, r)
-    case http.MethodDelete:
-        deletePerson(w, r)
-    default:
-        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-    }
+func queryHandler(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT * FROM mytable")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var results []map[string]interface{}
+	cols, _ := rows.Columns()
+	for rows.Next() {
+		columns := make([]interface{}, len(cols))
+		columnPointers := make([]interface{}, len(cols))
+		for i := range columns {
+			columnPointers[i] = &columns[i]
+		}
+		if err := rows.Scan(columnPointers...); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		result := make(map[string]interface{})
+		for i, colName := range cols {
+			val := columnPointers[i].(*interface{})
+			result[colName] = *val
+		}
+		results = append(results, result)
+	}
+
+	json.NewEncoder(w).Encode(results)
 }
 
-func getPersons(w http.ResponseWriter, r *http.Request) {
-    rows, err := db.Query("SELECT id, name, email FROM persons")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    defer rows.Close()
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	var requestData struct {
+		Data string `json:"data"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    persons := []Person{}
-    for rows.Next() {
-        var p Person
-        if err := rows.Scan(&p.ID, &p.Name, &p.Email); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-        persons = append(persons, p)
-    }
+	_, err := db.Exec("INSERT INTO mytable (data) VALUES (?)", requestData.Data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(persons)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Data added successfully"})
 }
 
-func addPerson(w http.ResponseWriter, r *http.Request) {
-    var p Person
-    if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	var requestData struct {
+		ID int `json:"id"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    result, err := db.Exec("INSERT INTO persons (name, email) VALUES (?, ?)", p.Name, p.Email)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	_, err := db.Exec("DELETE FROM mytable WHERE id = ?", requestData.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    id, err := result.LastInsertId()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-    p.ID = int(id)
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(p)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Data deleted successfully"})
 }
-
-func deletePerson(w http.ResponseWriter, r *http.Request) {
-    var p Person
-    if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
-
-    _, err := db.Exec("DELETE FROM persons WHERE id = ?", p.ID)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-    w.WriteHeader(http.StatusNoContent)
-}
-
