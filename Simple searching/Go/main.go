dc5b4951e934a2428d6bc6dcf30265393519e5bf
miyakiyu <sherry910509@gmail.com>
2024-07-17 14:24:27 +0800

    Fixing can't log in MySQL database problem

--- Simple searching/Go/main.go
+++ Simple searching/Go/main.go
@@ -12,16 +12,22 @@ import (
 )
 
 func main() {
-	dsn := "root:12345@tcp(127.0.0.1:3306)/Cats?charset=utf8mb4&parseTime=True&loc=Local"
+	// Globla setting
+	dsn := "root:12345@tcp(172.24.0.1:3306)/cat"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
 
 	r := gin.Default()
+	// Home Page
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "Hello World"})
+	})
 
+	// Get cats
 	r.GET("/cats", func(c *gin.Context) {
-		cats, err := models.GetAllCats(db)
+		cats, err := models.GetCats(db)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
